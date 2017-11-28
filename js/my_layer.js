b6356a7212219fafec726536465e9d4c17228c50
tntaiyu <tnt.2002@163.com>
2017-11-28 11:50:44 +0800

    优化弹窗

--- js/my_layer.js
+++ js/my_layer.js
@@ -1,6 +1,6 @@
-function my_layer(option,type,cbok,cbcancle){
+function my_layer(option, type, cbok, cbcancle) {
 
-    var  opt_def={title:'蓝信',icon:'/images/ic_tip1.png',message:'是否关闭当前窗口',tip:false};
+    var opt_def = {title: '蓝信', icon: '/images/ic_tip1.png', message: '是否关闭当前窗口', tip: false};
     var opt = jQuery.extend(opt_def, option);
 
     //邮箱
@@ -12,24 +12,24 @@ function my_layer(option,type,cbok,cbcancle){
         })
     }
 
-    var str='';
-    str+='<div class="mark"></div><div class="layer_box"  ><div class="layer_top" data-tran>'+opt.title+' <em></em></div>';
-    str+='<div class="layer_wrap"><div style="margin-left: 20px;margin-top: 20px;height: 34px;font-size: 0"><img src="'+opt.icon+'" alt="" style="display: inline-block;vertical-align: middle">'
-    str+='<span style="font-size:14px;color: #000; " data-tran>'+opt.message+'</span></div></div>'
+    var str = '';
+    str += '<div class="mark"></div><div class="layer_box"  ><div class="layer_top" data-tran>' + opt.title + ' <em></em></div>';
+    str += '<div class="layer_wrap"><div style="margin-left: 20px;margin-top: 20px;height: 34px;font-size: 0"><img src="' + opt.icon + '" alt="" style="display: inline-block;vertical-align: middle">'
+    str += '<span style="font-size:14px;color: #000; " data-tran>' + opt.message + '</span></div></div>'
     //str+='<div class="layer_footer"><em class="layer_check"></em><input type="checkbox" name="tip" id="tip"><label for="tip">不再提示</label>';
-    str+='<div class="layer_footer">';
+    str += '<div class="layer_footer">';
 
-   if(opt.tip){
+    if (opt.tip) {
 
-       str+='<em class="layer_check"></em><input type="checkbox" name="tip" class="tip"><label for="tip" data-tran>不再提示</label>'
-   }
-
-    if(type=='confirm'){
-        str+='<button  class="btn_n  btn_ok" data-tran>确定</button><button  class="btn_n btn_qx" data-tran>取消</button></div></div>';
-    }else{
-        str+='<button  class="btn_n  btn_ok" data-tran>确定</button></div></div>';
+        str += '<em class="layer_check"></em><input type="checkbox" name="tip" class="tip"><label for="tip" data-tran>不再提示</label>'
     }
-    var box=$(str).appendTo('body');
+
+    if (type == 'confirm') {
+        str += '<button  class="btn_n  btn_ok" data-tran>确定</button><button  class="btn_n btn_qx" data-tran>取消</button></div></div>';
+    } else {
+        str += '<button  class="btn_n  btn_ok" data-tran>确定</button></div></div>';
+    }
+    var box = $(str).appendTo('body');
 
     settrans(box)
 
@@ -37,75 +37,61 @@ function my_layer(option,type,cbok,cbcancle){
     //
     //    box.draggable({scroll: false})
     //}
+    if (opt.tip) {
+        $('.tip', box).change(function () {
+            if (this.checked) {
+                $('.layer_check', box).addClass('layer_checked')
 
+            } else {
+                $('.layer_check', box).removeClass('layer_checked')
+            }
+        });
 
-    $('.tip',box).change(function(){
-         if(this.checked){
-             $('.layer_check',box).addClass('layer_checked')
-
-         }else{
-             $('.layer_check',box).removeClass('layer_checked')
-         }
-    });
-
-    $('button',box).on('click',function(){
-        //$(this).addClass('btn_h').siblings('button').removeClass('btn_h');
-    })
-
+    }
 
 
     //类型为alert
+    var $img = $('.layer_wrap img', box),
+        $btn_qx = $('.btn_qx', box)
     if (type == 'success') {
-        $('.layer_wrap img',box).attr('src', './images/ic_tip4.png');
-        $('.btn_qx',box).hide();
+        $img.attr('src', './images/ic_tip4.png');
+        $btn_qx.hide();
 
+    } else if (type == 'error') {
+        $img.attr('src', './images/ic_tip3.png');
+        $btn_qx.hide();
+    } else if (type == 'warn') {
+        $img.attr('src', './images/ic_tip2.png');
+        $btn_qx.hide();
+    } else {
+        $img.attr('src', './images/ic_tip5.png');
     }
 
-    if (type == 'error') {
-        $('.layer_wrap img',box).attr('src', './images/ic_tip3.png');
-        $('.btn_qx',box).hide();
-    }
-
-    //类型为confirm
-    if (type == 'warn') {
-
-        $('.layer_wrap img',box).attr('src', './images/ic_tip2.png');
-        $('.btn_qx',box).hide();
-    }
-
-    //类型为confirm
-    if (type == 'confirm') {
-
-        $('.layer_wrap img',box).attr('src', './images/ic_tip5.png');
-    }
-
-
-    $('.btn_ok',box).click(function(){
+    $('.btn_ok', box).click(function () {
         box.remove();
-        if(Object.prototype.toString.call(cbok)=='[object Function]'){
+        if (Object.prototype.toString.call(cbok) == '[object Function]') {
 
             cbok();
         }
-
     })
 
 
-    $('.btn_qx',box).on('click',function(){
+    $('.btn_qx', box).on('click', function () {
         box.remove();
-        if(Object.prototype.toString.call(cbcancle)=='[object Function]'){
+        if (Object.prototype.toString.call(cbcancle) == '[object Function]') {
             cbcancle();
         }
     })
 
-    $('.layer_top em',box).click(function(){
+    $('.layer_top em', box).click(function () {
         box.remove();
 
-        if(type=='confirm'){
-            if(Object.prototype.toString.call(cbcancle)=='[object Function]'){
+        if (type == 'confirm') {
+            if (Object.prototype.toString.call(cbcancle) == '[object Function]') {
                 cbcancle();
             }
-        }else{
-            if(Object.prototype.toString.call(cbok)=='[object Function]'){
+        } else {
+            if (Object.prototype.toString.call(cbok) == '[object Function]') {
                 cbok();
             }
         }
@@ -113,10 +99,8 @@ function my_layer(option,type,cbok,cbcancle){
     })
 
 
-
-
 }
-function emial(ele){
+function emial(ele) {
     var parm = {email: ele.innerHTML};
     openEmail(parm)
 }
